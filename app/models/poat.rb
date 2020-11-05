dd7b32d4c404205b03629b3a60981d3a668d2bf6
kakiti <kakizaki3315chi@gmail.com>
2020-11-05 18:14:56 +0900

    first commit

---
+++ app/models/poat.rb
@@ -0,0 +1,2 @@
+class Poat < ApplicationRecord
+end
---
+++ db/migrate/20201027054313_practice.rb
@@ -0,0 +1,8 @@
+class CreatePractices < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+
+      t.timestamps
+    end
+  end
+end
